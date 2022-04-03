82b4134bd58f4edca5abdcdba51e1ff60984f753
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2022-04-03 11:10:39 +0530

    main

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const connect=async ()=>{
-    return mongoose.connect("mongodb://127.0.0.1:27017/shopperstop");
+    return mongoose.connect("mongodb+srv://prem:prem@cluster0.fkthv.mongodb.net/test");
 }
 
 module.exports=connect;
